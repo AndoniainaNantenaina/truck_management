a1bce031e242dde3127c706877cb06305a9515da
ChristianNomena <christiangrzk@gmail.com>
2022-04-09 09:12:45 +0300

    fix secretaire page

--- controllers/secretaire.js
+++ controllers/secretaire.js
@@ -1,10 +1,40 @@
 const pageSecretaire = async (req, res) => {
+    let data = {
+        cartes: [],
+        camions: [],
+        chauffeurs: [],
+        bc: []
+    };
+    console.log(process.env._HOST);
+    try {
+        let chauffeurs = await axios({
+            method: "get",
+            url: process.env._HOST + "/chauffeur_DataService/chauffeur",
+            dataType: "json"
+        });
+        data.chauffeurs = chauffeurs.data.chauffeurs.chauffeur;
+        data.chauffeurs.forEach(c => {
+            data.cartes.push(c.carte);
+        });
+
+        data.chauffeurs.forEach(c => {
+            data.camions.push(c.camion);
+        });
+
+        console.log(data)
+
+    } catch (e) {
+        console.log(e)
+    }
+
     res.render(
         'secretaire/secretaire.ejs',
         {
             mtitle: 'Truck management',
-            title: 'Secretaire'
-        });
+            title: 'Secretaire',
+            data
+        }
+    );
 }
 
 const postBC = async (req, res) => {
